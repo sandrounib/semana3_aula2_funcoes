a6738875bc7fab2f9695d2792da0b0f9759a2cc7
FIC <sandrounib@hotmail.com>
2017-10-18 21:51:45 -0200

    funcoes

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace semana3_aula2_funcoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+          /* */
+            System.Console.WriteLine("Digite seu nome: ");
+            string nome = Console.ReadLine();
+            System.Console.WriteLine("Digite sua idade: ");
+            string idade = Console.ReadLine();
+           /*
+           //FUNÇÃO 
+           //sem retorno e sem argumento
+
+            OlaMundo();
+            Console.WriteLine("----------------");
+            OlaMundo();
+            Console.WriteLine("----------------");
+        }
+
+        //função
+        static void OlaMundo(){
+            System.Console.WriteLine("Olá Mundo!");
+            System.Console.WriteLine("Tudo bem?");
+            System.Console.WriteLine("Como vai você.");
+            System.Console.WriteLine("Fim");
+        */
+
+        // FUNÇÃO SEM RETORNO E COM argumento
+
+        OlaMundoComArgumento(nome,idade);
+
+       
+        }
+        
+        
+        static void OlaMundoComArgumento(string nome, string idade){
+            System.Console.WriteLine("Seu nome é: " + nome);
+            System.Console.WriteLine("Sua idade é : " + idade );
+        }
+        
+    }
+}
---
+++ funcoes_com_retorno/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace funcoes_com_retorno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           System.Console.WriteLine("Digite o primeiro valor: ");
+           int valora =Convert.ToInt16(Console.ReadLine());
+
+           System.Console.WriteLine("Digite o Segundo valor: ");
+           int valorb =Convert.ToInt16(Console.ReadLine());
+           
+           System.Console.WriteLine("Digite o tipo de operação: ");
+           string operação = Console.ReadLine();            
+
+          int resultado = Calculadora(valora, valorb,operação);
+          System.Console.WriteLine("O resultado é: " + resultado);
+
+
+            
+            /*
+            double valor = ValorPI();
+            System.Console.WriteLine("Valor de pi é : " + valor);
+
+        }
+        //Função com funcoes_com_retorno
+        static double ValorPI(){
+
+            return Math.PI;
+         */
+        
+        /*
+        string cpr = GerarCpf();
+        System.Console.WriteLine("CPF Gerado: " + GerarCpf());
+
+        }
+         static string GerarCpf(){
+             string cpf ="08929532845";
+             //
+             return cpf;
+            
+         }
+          */
+        }
+        static int Calculadora(int a, int b, string tipo){
+            int valor = 0;
+            switch(tipo){
+                case "+" :
+                    valor = a + b;
+                    break;
+                case "-" :
+                    valor = a - b;
+                    break;
+                case "*" :
+                    valor = a * b;
+                    break;
+                case "/" :
+                    valor = a / b;
+                    break;                
+            }
+            return valor;
+        }
+          
+    }
+}
